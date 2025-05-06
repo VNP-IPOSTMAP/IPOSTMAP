344f731ceee91df6ca18e8e1587ed5b5322b94c2
VNP-IPOSTMAP <landm1@vnpost.vn>
2025-05-06 07:31:47 +0700

    Delete public directory

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var wWidth = $(window).width();
-    if (wWidth >= 992) {
-      $("#itemDropdown").hover(function () {
-        $(this).children("ul").slideToggle(300);
-      });
-    } 
-  });
